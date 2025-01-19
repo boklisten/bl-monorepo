f235a667e1b4d5d9e44821533c2c7e8f1d10e7eb
Adrian Andersen <adrianandersen@protonmail.com>
2025-01-19 11:08:06 +0100

    build: do noe enable sentry in dev mode

--- backend/src/instrument.ts
+++ backend/src/instrument.ts
@@ -1,10 +1,12 @@
 import { assertEnv, BlEnvironment } from "@backend/config/environment";
 import * as Sentry from "@sentry/node";
+import { mongooseIntegration } from "@sentry/node";
 import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
 Sentry.init({
   dsn: "https://7a394e7cab47142de06327e453c54837@o569888.ingest.us.sentry.io/4508653655293952",
-  integrations: [nodeProfilingIntegration()],
+  integrations: [nodeProfilingIntegration(), mongooseIntegration()],
   tracesSampleRate: 1.0,
-  enabled: assertEnv(BlEnvironment.API_ENV) === "production",
+  environment: assertEnv(BlEnvironment.API_ENV),
+  enabled: assertEnv(BlEnvironment.API_ENV) !== "dev",
 });

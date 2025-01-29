a697516a8b07ae0c14923b12e850d1423d2b79f3
Adrian Andersen <adrianandersen@protonmail.com>
2025-01-29 11:25:35 +0100

    feat: capture errors to sentry in error handler

--- backend/app/exceptions/handler.ts
+++ backend/app/exceptions/handler.ts
@@ -1,5 +1,6 @@
 import { HttpContext, ExceptionHandler } from "@adonisjs/core/http";
 import app from "@adonisjs/core/services/app";
+import * as Sentry from "@sentry/node";
 
 export default class HttpExceptionHandler extends ExceptionHandler {
   protected override debug = !app.inProduction;
@@ -7,6 +8,7 @@ export default class HttpExceptionHandler extends ExceptionHandler {
     return super.handle(error, ctx);
   }
   override async report(error: unknown, ctx: HttpContext) {
+    Sentry.captureException(error);
     return super.report(error, ctx);
   }
 }

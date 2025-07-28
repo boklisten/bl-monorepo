0e8a4ccb2517d5326287887e0b93cc4ac56f2930
Adrian Andersen <adrianandersen@protonmail.com>
2025-07-28 21:46:14 +0200

    feat: enable Vipps button in prod

--- frontend/src/components/user/VippsButton.tsx
+++ frontend/src/components/user/VippsButton.tsx
@@ -5,9 +5,6 @@ import useSocialLogin from "@/utils/useSocialLogin";
 export default function VippsButton({ verb }: { verb: "login" | "register" }) {
   const { redirectToLogin } = useSocialLogin();
 
-  // fixme: remove this to enable Vipps-login in production (waiting for API keys from Vipps)
-  if (process.env["NEXT_PUBLIC_APP_ENV"] === "production") return <></>;
-
   return (
     <Box onClick={() => redirectToLogin("vipps")}>
       {/* @ts-expect-error official Vipps button */}

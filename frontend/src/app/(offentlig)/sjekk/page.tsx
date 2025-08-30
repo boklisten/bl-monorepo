dc44f620782b3b9c9cadb32d012555d34cac9b5b
Adrian Andersen <adrianandersen@protonmail.com>
2025-08-30 22:44:42 +0200

    chore: migrate public blid search to Mantine

--- frontend/src/app/(offentlig)/sjekk/page.tsx
+++ frontend/src/app/(offentlig)/sjekk/page.tsx
@@ -1,4 +1,4 @@
-import { Container, Stack, Title } from "@mantine/core";
+import { Container, Stack, Title, Text } from "@mantine/core";
 import { Metadata } from "next";
 
 import AuthGuard from "@/components/common/AuthGuard";
@@ -11,9 +11,13 @@ export const metadata: Metadata = {
 
 export default function PublicBlidSearchPage() {
   return (
-    <Container size={"sm"}>
+    <Container size={"xs"}>
       <Stack>
-        <Title>Boksøk</Title>
+        <Title ta={"center"}>Boksøk</Title>
+        <Text ta={"center"}>
+          Skriv inn en bok sin unike ID (8 eller 12 siffer) for å se hvem den
+          tilhører. Du kan også scanne bokas unike ID med kamera.
+        </Text>
         <AuthGuard>
           <PublicBlidSearch />
         </AuthGuard>
--- frontend/src/components/search/PublicBlidSearch.tsx
+++ frontend/src/components/search/PublicBlidSearch.tsx
@@ -1,19 +1,18 @@
 "use client";
 import { PublicBlidLookupResult } from "@boklisten/backend/shared/public_blid_lookup";
-import { Stack, Text } from "@mantine/core";
-import { Phone } from "@mui/icons-material";
-import EmailIcon from "@mui/icons-material/Email";
-import QrCodeScannerIcon from "@mui/icons-material/QrCodeScanner";
 import {
+  ActionIcon,
+  Card,
+  Group,
+  Stack,
   Table,
-  TableBody,
-  TableCell,
-  TableRow,
-  Typography,
-  Box,
-} from "@mui/material";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
+  Text,
+  TextInput,
+  Title,
+  Tooltip,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { IconMail, IconObjectScan, IconPhone } from "@tabler/icons-react";
 import moment from "moment";
 import { useState } from "react";
 
@@ -22,7 +21,7 @@ import useApiClient from "@/utils/api/useApiClient";
 
 export default function PublicBlidSearch() {
   const client = useApiClient();
-  const [scannerModalOpen, setScannerModalOpen] = useState(false);
+  const [isOpen, { open, close }] = useDisclosure();
   const [blidSearch, setBlidSearch] = useState("");
   const [searchResult, setSearchResult] = useState<
     PublicBlidLookupResult | "inactive" | null
@@ -30,7 +29,7 @@ export default function PublicBlidSearch() {
 
   async function onBlidSearch(blid: string) {
     setSearchResult(null);
-    setScannerModalOpen(false);
+    close();
     setBlidSearch(blid);
     if (blid.length !== 8 && blid.length !== 12) {
       // Only lookup for valid blids
@@ -54,144 +53,87 @@ export default function PublicBlidSearch() {
 
   return (
     <>
-      <Text>
-        Skriv inn en bok sin unike ID (8 eller 12 siffer) for å se hvem den
-        tilhører. Du kan også trykke på{" "}
-        <QrCodeScannerIcon fontSize={"small"} sx={{ my: -0.7 }} /> for å scanne
-        bokas unike ID med kamera.
+      <TextInput
+        label={"Unik ID"}
+        onChange={(event) => onBlidSearch(event.target.value)}
+        rightSection={
+          <Tooltip label={"Åpne scanner"}>
+            <ActionIcon variant={"subtle"} onClick={open}>
+              <IconObjectScan />
+            </ActionIcon>
+          </Tooltip>
+        }
+      />
+      <Text c={"gray"} size={"sm"} ta={"center"}>
+        {searchResult === "inactive" &&
+          "Denne boken er ikke registrert som utdelt."}
+        {((searchResult === null &&
+          blidSearch.length !== 8 &&
+          blidSearch.length !== 12) ||
+          blidSearch.length === 0) &&
+          "Venter på unik ID"}
       </Text>
-      <Stack>
-        <TextField
-          label={"Unik ID"}
-          value={blidSearch}
-          onChange={(event) => onBlidSearch(event.target.value)}
-          slotProps={{
-            input: {
-              endAdornment: (
-                <Button onClick={() => setScannerModalOpen(true)}>
-                  <QrCodeScannerIcon />
-                </Button>
-              ),
-            },
-          }}
-        />
-        <Typography
-          variant="body2"
-          sx={{
-            textAlign: "center",
-            color: "gray",
-            fontSize: 14,
-            mb: 1,
-          }}
-        >
-          {searchResult === "inactive" &&
-            "Denne boken er ikke registrert som utdelt."}
-          {((searchResult === null &&
-            blidSearch.length !== 8 &&
-            blidSearch.length !== 12) ||
-            blidSearch.length === 0) &&
-            "Venter på unik ID"}
-        </Typography>
-        {searchResult !== "inactive" && searchResult !== null && (
-          <>
-            <Typography
-              variant={"h5"}
-              sx={{
-                textAlign: "center",
-                mt: 0,
-              }}
-            >
-              Denne boken tilhører
-            </Typography>
-            <Box
-              sx={{
-                padding: 2,
-                border: "1px solid #ddd",
-                borderRadius: 2,
-                maxWidth: 500,
-                margin: "0 auto",
-              }}
-            >
-              <Typography
-                variant="h5"
-                gutterBottom
-                sx={{
-                  textAlign: "center",
-                }}
-              >
+      {searchResult !== "inactive" && searchResult !== null && (
+        <>
+          <Text size={"xl"} ta={"center"}>
+            Denne boken tilhører
+          </Text>
+          <Card shadow="xl" withBorder>
+            <Stack>
+              <Title order={4} ta={"center"}>
                 {searchResult.name}
-              </Typography>
+              </Title>
+              <Stack align={"center"} gap={"xs"}>
+                <Group gap={5} justify={"center"}>
+                  <IconPhone />
+                  <Text>{searchResult.phone}</Text>
+                </Group>
+                <Group gap={5} justify={"center"}>
+                  <IconMail />
+                  <Text>{searchResult.email}</Text>
+                </Group>
+              </Stack>
 
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  gap: 1,
-                  mt: 2,
-                  px: 5,
-                }}
-              >
-                <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-                  <Phone fontSize="small" sx={{ color: "primary.main" }} />
-                  <Typography variant="body1">{searchResult.phone}</Typography>
-                </Box>
-                <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-                  <EmailIcon fontSize="small" sx={{ color: "primary.main" }} />
-                  <Typography variant="body1">{searchResult.email}</Typography>
-                </Box>
-              </Box>
-
-              <Table sx={{ mt: 2 }}>
-                <TableBody>
-                  <TableRow>
-                    <TableCell>
-                      <b>Tittel</b>
-                    </TableCell>
-                    <TableCell>{searchResult.title}</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>
-                      <b>ISBN</b>
-                    </TableCell>
-                    <TableCell>{searchResult.isbn}</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>
-                      <b>Utdelt hos</b>
-                    </TableCell>
-                    <TableCell>{searchResult.handoutBranch}</TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>
-                      <b>Utdelt den</b>
-                    </TableCell>
-                    <TableCell>
+              <Table verticalSpacing={"md"} ta={"left"} layout={"fixed"}>
+                <Table.Tbody>
+                  <Table.Tr>
+                    <Table.Th>Tittel</Table.Th>
+                    <Table.Td>{searchResult.title}</Table.Td>
+                  </Table.Tr>
+                  <Table.Tr>
+                    <Table.Th>ISBN</Table.Th>
+                    <Table.Td>{searchResult.isbn}</Table.Td>
+                  </Table.Tr>
+                  <Table.Tr>
+                    <Table.Th>Utdelt hos</Table.Th>
+                    <Table.Td>{searchResult.handoutBranch}</Table.Td>
+                  </Table.Tr>
+                  <Table.Tr>
+                    <Table.Th>Utdelt den</Table.Th>
+                    <Table.Td>
                       {moment(searchResult.handoutTime).format("DD/MM/YYYY")}
-                    </TableCell>
-                  </TableRow>
-                  <TableRow>
-                    <TableCell>
-                      <b>Frist</b>
-                    </TableCell>
-                    <TableCell>
+                    </Table.Td>
+                  </Table.Tr>
+                  <Table.Tr>
+                    <Table.Th>Frist</Table.Th>
+                    <Table.Td>
                       {moment(searchResult.deadline).format("DD/MM/YYYY")}
-                    </TableCell>
-                  </TableRow>
-                </TableBody>
+                    </Table.Td>
+                  </Table.Tr>
+                </Table.Tbody>
               </Table>
-            </Box>
-          </>
-        )}
-        <ScannerModal
-          onScan={async (blid) => {
-            onBlidSearch(blid);
-            return [{ feedback: "" }] as [{ feedback: string }];
-          }}
-          open={scannerModalOpen}
-          handleClose={() => setScannerModalOpen(false)}
-        />
-      </Stack>
+            </Stack>
+          </Card>
+        </>
+      )}
+      <ScannerModal
+        onScan={async (blid) => {
+          onBlidSearch(blid);
+          return [{ feedback: "" }] as [{ feedback: string }];
+        }}
+        open={isOpen}
+        handleClose={close}
+      />
     </>
   );
 }

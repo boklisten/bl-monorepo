7bb871b34fda09197a4ec78c8a18cc537228ecd3
Adrian Andersen <adrianandersen@protonmail.com>
2025-10-25 17:56:16 +0200

    refactor: start adopting Activities

--- frontend/src/shared/components/OrderCard.tsx
+++ frontend/src/shared/components/OrderCard.tsx
@@ -7,6 +7,7 @@ import {
   IconSignature,
 } from "@tabler/icons-react";
 import dayjs from "dayjs";
+import { Activity } from "react";
 
 import WarningAlert from "@/shared/components/alerts/WarningAlert";
 import OrderItemTypeIcon from "@/shared/components/OrderItemTypeIcon";
@@ -38,17 +39,18 @@ export default function OrderCard({
           </Title>
           <Text>{dayjs(creationTime).format("DD/MM/YYYY HH:mm:ss")}</Text>
         </Group>
-        {pendingSignature ? (
+        <Activity mode={pendingSignature ? "visible" : "hidden"}>
           <WarningAlert title={"Denne orderen krever gyldig signatur"}>
             Du må ha en gyldig signatur før du kan motta disse bøkene. Sjekk
             dine oppgaver for mer informasjon
           </WarningAlert>
-        ) : (
+        </Activity>
+        <Activity mode={!pendingSignature ? "visible" : "hidden"}>
           <Group gap={5}>
             <IconSignature />
             <Text c={"dimmed"}>Denne orderen krever ikke signering</Text>
           </Group>
-        )}
+        </Activity>
         <Stack gap={"xs"}>
           <Title order={5}>Bøker</Title>
           {orderItems.map((orderItem) => (
@@ -56,41 +58,44 @@ export default function OrderCard({
               <Stack gap={5}>
                 <Group justify={"space-between"}>
                   <Title order={6}>{orderItem.title}</Title>
-                  {orderItem.unitPrice && (
+                  <Activity mode={orderItem.amount ? "visible" : "hidden"}>
                     <Text>{Math.abs(orderItem.unitPrice)} kr</Text>
-                  )}
+                  </Activity>
                 </Group>
-                {orderItem.blid && (
+                <Activity mode={orderItem.blid ? "visible" : "hidden"}>
                   <Group gap={2}>
                     <IconQrcode size={18} />
                     <Text>{orderItem.blid}</Text>
                   </Group>
-                )}
+                </Activity>
                 <Group gap={2}>
                   <OrderItemTypeIcon type={orderItem.type} />
                   <Text>{orderItem.typeLabel}</Text>
-                  {["rent", "partly-payment", "loan", "extend"].includes(
-                    orderItem.type,
-                  ) &&
-                    orderItem.info?.to && (
-                      <>
-                        til
-                        <Text size={"sm"} fw={"bold"}>
-                          {dayjs(orderItem.info?.to).format("DD/MM/YYYY")}
-                        </Text>
-                      </>
-                    )}
+                  <Activity
+                    mode={
+                      ["rent", "partly-payment", "loan", "extend"].includes(
+                        orderItem.type,
+                      ) && orderItem.info?.to
+                        ? "visible"
+                        : "hidden"
+                    }
+                  >
+                    til
+                    <Text size={"sm"} fw={"bold"}>
+                      {dayjs(orderItem.info?.to).format("DD/MM/YYYY")}
+                    </Text>
+                  </Activity>
                 </Group>
-                {orderItem.movedToOrder && (
+                <Activity mode={orderItem.movedToOrder ? "visible" : "hidden"}>
                   <Text c={"dimmed"}>
                     Denne boken har blitt flyttet til en annen ordre.
                   </Text>
-                )}
+                </Activity>
               </Stack>
             </Card>
           ))}
         </Stack>
-        {payments.length > 0 && (
+        <Activity mode={payments.length > 0 ? "visible" : "hidden"}>
           <Stack gap={5}>
             <Title order={5}>Betaling</Title>
             {payments.map((payment) => (
@@ -101,23 +106,20 @@ export default function OrderCard({
                     {Math.abs(payment.amount)} kr med {payment.methodLabel}
                   </Text>
                   <Group gap={5}>
-                    {payment.confirmed ? (
-                      <>
-                        <IconCheck color={"green"} />
-                        <Text c={"green"}>Bekreftet</Text>
-                      </>
-                    ) : (
-                      <>
-                        <IconExclamationMark />
-                        <Text>Bekreftet</Text>
-                      </>
-                    )}
+                    <Activity mode={payment.confirmed ? "visible" : "hidden"}>
+                      <IconCheck color={"green"} />
+                      <Text c={"green"}>Bekreftet</Text>
+                    </Activity>
+                    <Activity mode={!payment.confirmed ? "visible" : "hidden"}>
+                      <IconExclamationMark />
+                      <Text>Bekreftet</Text>
+                    </Activity>
                   </Group>
                 </Group>
               </Card>
             ))}
           </Stack>
-        )}
+        </Activity>
       </Stack>
     </Card>
   );

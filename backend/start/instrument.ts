964a0dc58789bfacf135b8602d6887fa9aa43e70
Adrian Andersen <adrianandersen@protonmail.com>
2026-03-18 15:05:05 +0100

    chore: bump deps

--- backend/start/instrument.ts
+++ backend/start/instrument.ts
@@ -1,13 +1,12 @@
 import * as Sentry from "@sentry/node";
 import { mongooseIntegration } from "@sentry/node";
-import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
 import env from "#start/env";
 
 if (env.get("API_ENV") !== "test") {
   Sentry.init({
     dsn: "https://7a394e7cab47142de06327e453c54837@o569888.ingest.us.sentry.io/4508653655293952",
-    integrations: [nodeProfilingIntegration(), mongooseIntegration()],
+    integrations: [mongooseIntegration()],
     tracesSampleRate: 1.0,
     environment: env.get("API_ENV"),
     enabled: env.get("API_ENV") !== "dev",

61a53477a05a4ae3aee7097fd47bc6c77f7724fe
Adrian Andersen <adrianandersen@protonmail.com>
2026-03-20 09:46:32 +0100

    fix: remove /production form script

--- cron_jobs/copy_prod_db_to_staging/dump_and_restore.sh
+++ cron_jobs/copy_prod_db_to_staging/dump_and_restore.sh
@@ -1,2 +1,2 @@
-mongodump --uri="${FROM_MONGODB_URI}/production"
+mongodump --uri="${FROM_MONGODB_URI}"
 mongorestore --drop --uri="${TO_MONGODB_URI}" --nsFrom="production.*" --nsTo="staging.*" dump/
